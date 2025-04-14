7f333d2d4d9571cd257d1a99824da635529fdf7f
a7eul <lizamakarova262008@gmail.com>
2025-04-14 21:45:01 +0400

    Задание 10

---
+++ src/Task10.java
@@ -0,0 +1,14 @@
+public class Task10 {
+    public static void main(String[] args) {
+        System.out.println("Задание № 10");
+        final int HEIGHT = 200;
+    final int WEIGHT = 90;
+    int idealWeight = HEIGHT - 110; // 75
+        int nWeight = idealWeight - WEIGHT;
+    System.out.println("Недовес: меньше чем 18.5");
+        System.out.println("Нормальный: между 18.5 и 24.9");
+        System.out.println("Избыточный вес: между 25 и 29.9");
+        System.out.println("Ожирение: 30 или больше");
+
+    }
+}
